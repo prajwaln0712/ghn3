cd37165d9a251c981ecbe8bf89be8c3099817801
prajwaln0712 <sams.prajwal@gmail.com>
2026-04-12 11:26:01 -0400

    one last test

--- eval_ghn.py
+++ eval_ghn.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 """
-Evaluates a GHN on one or all PyTorch models on ImageNet. Testing 20012001
+Evaluates a GHN on one or all PyTorch models on ImageNet. Trying one last time
 This script assumes the ImageNet dataset is already downloaded and set up as described in scripts/imagenet_setup.sh.
 
 Example
